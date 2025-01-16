01e80a573844e935636130a147188817a0f06765
Michael Gruper <sarge6411@icloud.com>
2025-01-16 08:00:54 -0600

    inital commit

--- MovieReleaseYearGruper/ViewController.swift
+++ MovieReleaseYearGruper/ViewController.swift
@@ -8,34 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let movieName = ""
     @IBOutlet weak var ghostLabelOutlet: UILabel!
     
+    @IBOutlet weak var textFieldOutlet: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        getMovie()
     }
     func getMovie(){
         let session = URLSession.shared
-        let movieURL = URL(string: "http://www.omdbapi.com/?i=tt3896198&apikey=62536319")!
-        
-        let dataTask = session.dataTask(with: movieURL) { data, response, error in
-            if let e = error{
-                print("Error! \(e)")
-            }
-            else{
-                if let d = data{
-                    if let jsonObj = try? JSONSerialization.jsonObject(with: d, options: .fragmentsAllowed) as? NSDictionary {
-                        print(jsonObj)
-                        DispatchQueue.main.async{
-                            self.ghostLabelOutlet.text = "\(jsonObj.value(forKey: "Title")!)"
+        let movieName = textFieldOutlet.text!
+        if movieName != ""{
+            let movieURL = URL(string: "http://www.omdbapi.com/?i=tt3896198&apikey=62536319&t=\(movieName)")!
+            let dataTask = session.dataTask(with: movieURL) { data, response, error in
+                if let e = error{
+                    print("Error! \(e)")
+                }
+                else{
+                    if let d = data{
+                        if let jsonObj = try? JSONSerialization.jsonObject(with: d, options: .fragmentsAllowed) as? NSDictionary {
+                            print(jsonObj)
+                            let j = jsonObj.value(forKey: "Title")!
+                            DispatchQueue.main.async{
+                                self.ghostLabelOutlet.text = "\(j)"
+                            }
                         }
                     }
                 }
+                
             }
-            
+            dataTask.resume()
         }
-        dataTask.resume()
+    }
+    @IBAction func searchButtonAction(_ sender: UIButton) {
+        getMovie()
     }
 }
 
